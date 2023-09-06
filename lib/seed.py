a55312dab6e1d882cf91e7046978451dd455db5b
Mr.Perfect 092 <135037455+stephan-maina@users.noreply.github.com>
2023-09-06 11:58:21 +0300

    Done.

--- lib/seed.py
+++ lib/seed.py
@@ -1,3 +1,23 @@
 #!/usr/bin/env python3
 
-# Script goes here!
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from models import Base, User, Post
+
+engine = create_engine('postgresql://username:password@localhost/database_name')
+Base.metadata.create_all(engine)
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+# Seed data
+user1 = User(username="user1")
+user2 = User(username="user2")
+
+post1 = Post(title="Post 1", content="Content of Post 1", author=user1)
+post2 = Post(title="Post 2", content="Content of Post 2", author=user2)
+
+session.add_all([user1, user2, post1, post2])
+session.commit()
+
+session.close()
